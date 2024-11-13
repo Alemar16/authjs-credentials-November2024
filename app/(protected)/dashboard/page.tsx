e5577b369e238a3e021e250f41bc46913c2d07d5
Armando Martinez <alemar.martinez16@gmail.com>
2024-11-13 16:05:59 -0300

    style(dashboard): update visual presentation styles on dashboardPage

--- app/(protected)/dashboard/page.tsx
+++ app/(protected)/dashboard/page.tsx
@@ -9,9 +9,25 @@ export default async function DashboardPage() {
   }
 
   return (
-    <div className="container">
-      <pre>{JSON.stringify(session, null, 2)}</pre>
-      <LogoutButton />
+    <div className="container mx-auto p-8">
+      <div className="text-center mb-8">
+        <h1 className="text-4xl font-bold mb-2">Dashboard</h1>
+      </div>
+
+      <p className="text-lg text-gray-700">
+        Este dashboard es el resultado de tu registro o inicio de sesión. Aquí
+        puedes ver la información de tu sesión y realizar acciones adicionales.
+      </p>
+
+      <div className="bg-white p-6 rounded-lg shadow-lg overflow-auto">
+        <pre className="text-sm text-gray-700">
+          {JSON.stringify(session, null, 2)}
+        </pre>
+      </div>
+
+      <div className="mt-8 text-center">
+        <LogoutButton />
+      </div>
     </div>
   );
 }
